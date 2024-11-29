52fc4003ef53b9c02df04a22fbc1d88b4616bdf5
StanMandela <smstan8@gmail.com>
2024-11-29 16:33:32 +0300

    Day 2: Starting typescript

---
+++ vueTypscript/my-vue-app/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="app">
+   {{name}}-{{ age }}
+   <button @click="changeName('Imelda')">Change Name</button>
+   <button @click="changeAge(30)">Change Age</button>
+
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'App',
+  components: {},
+  data(){
+    return{
+      name:'hkjjs',
+      age:24 as number|string
+    }
+  },
+  methods:{
+    changeName(name :number){
+      this.name= name
+    },
+    changeAge(age:number|string|boolean){
+      this.age = age
+
+    }
+  }
+});
+</script>
+
+<style>
+
+</style>
